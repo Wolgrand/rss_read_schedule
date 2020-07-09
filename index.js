f4c2e251d83a021e8ce785115ab0ea4da4ffe850
wolgrand <wolgrand@live.com>
2020-07-09 16:11:30 +0000

    update files

--- index.js
+++ index.js
@@ -19,35 +19,43 @@ function spider(url, source, thumbnail) {
             pubDate: feed.pubDate
           })
           .then(res => {
-            console.log(`statusCode: ${res.statusCode}`);
-            console.log(res);
+            console.log("Deu certo " + source + " " + new Date());
+           
           })
           .catch(error => {
-            console.error(error);
+            console.error("Erro " + source + " " + new Date());
           })
       )
     );
-  console.log("Foi");
+  
 }
 
 cron.schedule(
-  "* * * * *",
+  " */10 * * * * ",
   () =>
     spider(
       "https://api.rss2json.com/v1/api.json?rss_url=https://pt.aleteia.org/feed/",
       "Aleteia",
       "https://secure.gravatar.com/blavatar/3d32b741af79fa4cf43d6c2fd3dc7ab3?s=96&amp;d=https%3A%2F%2Fs0.wp.com%2Fi%2Fbuttonw-com.png"
-    ),
+    ));
+cron.schedule(
+	" */10 * * * * ",
+	() =>
   spider(
     "https://api.rss2json.com/v1/api.json?rss_url=https://www.vaticannews.va/pt.rss.xml",
     "Vatican News",
     "https://www.vaticannews.va/etc/designs/vatican-news/release/library/main/images/favicons/apple-icon-180x180.png"
-  ),
+  ));
+cron.schedule(
+	" */10 * * * * ",
+	() =>
   spider(
     "https://api.rss2json.com/v1/api.json?rss_url=https://www.acidigital.com/rss/rss.php",
     "Aci Digital",
     "https://www.acidigital.com/images/acilogo.png"
-  ),
+  ));
+cron.schedule(
+	" */10 * * * * ",
   spider(
     "https://api.rss2json.com/v1/api.json?rss_url=http://feeds.feedburner.com/churchpopportugues?format=xml",
     "Church Pop PT",
